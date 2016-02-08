561bd9bc0c9a5aed066711b459a84d1f57aa5fcd
Shirish Kamath <shirishk.87@gmail.com>
2016-02-08 14:02:24 +0530

    Added project, build to conf

--- tests/helper.js
+++ tests/helper.js
@@ -36,6 +36,9 @@ function runRepository(testHome, runnerPath, repository, conf, callback) {
     var projectPath = path.resolve(path.join(repository.name + '-' + repository.branch));
     conf.test_framework = repository.test_framework;
     conf.browsers = repository.browsers;
+    conf.project = repository.name;
+    conf.build = repository.branch;
+
     conf.test_path = conf.test_path || [];
     (repository.test_path || []).forEach(function (p) {
       conf.test_path.push(p);
